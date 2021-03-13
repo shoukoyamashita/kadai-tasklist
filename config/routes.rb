3af29ed60d0fd47eac0d9ecc08eb7d6da191b5da
shouko.yamashita <white_lemontea@yahoo.co.jp>
2021-03-13 04:01:48 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
   root to: 'tasks#index'
 
+  get 'signup', to: 'users#new'
   resources :tasks
+  resources :users, only: [:index, :show, :create]
 
 end
