07b2dd1f8cfeac5d02c9c580693ed0d46bd931a0
shouko.yamashita <white_lemontea@yahoo.co.jp>
2021-03-14 06:03:58 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,13 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+# before_action :correct_user, only: [:destroy]
   before_action :set_task, only: [:show, :edit, :update, :destroy] 
   
   def index
-    @tasks = Task.order(id: :desc).page(params[:page]).per(10)
+    if logged_in?
+    #@task = current_user.tasks.build  # form_with 用
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(10)
+    end
   end
 
   def show
@@ -13,14 +18,14 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
-
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'Task が正常に登録されました'
-      redirect_to @task
+      redirect_to root_url #@task
     else
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'Task が登録されませんでした'
-      render :new
+      render 'tasks/index'
     end    
   end
 
@@ -53,4 +58,13 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:content, :status)
   end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
+  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
 Rails.application.routes.draw do
+  root to: 'tasks#index'
+  
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   get 'users/index'
   get 'users/show'
   get 'users/new'
   get 'users/create'
-  root to: 'tasks#index'
 
   get 'signup', to: 'users#new'
-  resources :tasks
   resources :users, only: [:index, :show, :create]
 
+  resources :tasks
 end
